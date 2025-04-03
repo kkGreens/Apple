dfcabbff5a050edac90a5942dc18f51c7f3ab4c6
kk <karkuzhaliomrbranch@gmail.com>
2025-04-03 18:07:42 +0530

    initial commit

---
+++ src/org/com/Sample.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Sample {
+	
+	public void m1() {
+		System.out.println("Apple");
+	}
+	
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.m1();
+	}
+
+}
